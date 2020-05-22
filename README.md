500a26e12323e698ce0595c4dec9a213d5cc5b27
Mireiawen Rose <mir@mireiawen.net>
2020-05-22 15:06:45 +0300

    Set the admin password

--- README.md
+++ README.md
@@ -7,11 +7,14 @@ To start the container, just run something like the following:
 docker 'run' \
 	--detach \
 	--rm \
+	--env 'ADMIN_PASSWORD=secret' \
 	--name 'openlitespeed' \
 	--publish '80:80' \
 	--publish '7080:7080' \
 	'mireiawen/openlitespeed'
 ```
+
+To set the webadmin password, use the environment variable `ADMIN_PASSWORD` to set it.
 
 The container has 2 ports exposed, the HTTP port and the Web Admin interface running HTTPS on 7080.
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@ set -e
 # Start the LiteSpeed
 /usr/local/lsws/bin/litespeed
 
+# Update the credentials
+if [ -n "${ADMIN_PASSWORD}" ]
+then
+	ENCRYPT_PASSWORD="$(/usr/local/lsws/admin/fcgi-bin/admin_php -q '/usr/local/lsws/admin/misc/htpasswd.php' "${ADMIN_PASSWORD}")"
+	echo "admin:${ENCRYPT_PASSWORD}" >'/usr/local/lsws/admin/conf/htpasswd'
+	echo "WebAdmin user/password is admin/${ADMIN_PASSWORD}" >'/usr/local/lsws/adminpasswd'
+fi
+
 # Read the credentials
 cat '/usr/local/lsws/adminpasswd'
 
